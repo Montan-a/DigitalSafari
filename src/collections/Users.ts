cddc6456273bf02c44f583e5ea785d0828c669cb
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-01-28 17:44:08 -0600

    initial commit

---
+++ src/collections/Users.ts
@@ -0,0 +1,52 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+  slug: "users",
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify account</a>`;
+      },
+    },
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
+    {
+      name: "role",
+      defaultValue: "user",
+      required: true,
+
+      type: "select",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        { label: "User", value: "user" },
+      ],
+    },
+  ],
+};
