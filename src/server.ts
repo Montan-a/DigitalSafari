faef46902e574cce3e3490bdbfbe57eda24a322c
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-02-19 10:46:21 -0600

    cart page is exclusive to signed in users

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import { IncomingMessage } from "http";
 import { stripeWebhookHandler } from "./webhooks";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -38,7 +40,17 @@ const start = async () => {
       },
     },
   });
+  const cartRouter = express.Router();
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+    if (!request.user) {
+      return res.redirect("/sign-in?origin=cart");
+    }
+    const parseUrl = parse(req.url, true);
 
+    return nextApp.render(req, res, "/cart", parseUrl.query);
+  });
+  app.use("/cart", cartRouter);
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info("Next.js is building for production");
