9d45b3eae6f17b0a25106811496e266804cc7b87
Montana Shaw <88953023+Montan-a@users.noreply.github.com>
2024-02-19 14:18:45 -0600

    Added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white flex-grow-0">
+      <MaxWidthWrapper>
+        <div className="border-t border-gray-200"></div>
+      </MaxWidthWrapper>
+    </footer>
+  );
+};
